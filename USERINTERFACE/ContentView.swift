d2b81cb6a062347f9693a06882155a2cfb3becc8
Makayla E <Strawberryanimation10186@gmail.com>
2023-06-23 10:21:40 -0400

    Initial Commit

---
+++ USERINTERFACE/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  USERINTERFACE
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("BLACK PUNK")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.567, saturation: 0.691, brightness: 0.626))
+            Image("RICON")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ USERINTERFACE/USERINTERFACEApp.swift
@@ -0,0 +1,17 @@
+//
+//  USERINTERFACEApp.swift
+//  USERINTERFACE
+//
+//  Created by Scholar on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct USERINTERFACEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
